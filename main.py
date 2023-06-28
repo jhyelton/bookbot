7a1051aa3f3b852bc540911106faa21dc54b86b0
Josh Yelton <jyelton@transfrvr.com>
2023-06-28 13:45:43 -0500

    feat: count words

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import os
 def main():
     with open(os.path.join(os.path.dirname(__file__),"books/frankenstein.txt")) as f:
         file_contents = f.read()
-        print(file_contents)
+        print(word_count(file_contents))
 
+
+def word_count(text):
+    return len(text.split())
 
 if __name__ == '__main__':
     main()
