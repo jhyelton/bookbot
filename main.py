2e1cebb132dd24863d0cd6f7aafaa610aab69b0b
Josh Yelton <jyelton@transfrvr.com>
2023-06-28 13:47:10 -0500

    feat: count characters

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ import os
 def main():
     with open(os.path.join(os.path.dirname(__file__),"books/frankenstein.txt")) as f:
         file_contents = f.read()
-        print(word_count(file_contents))
+        print(char_count(file_contents))
 
 
 def word_count(text):
     return len(text.split())
+
+def char_count(text):
+    char_map = {}
+    for char in text.lower():
+        if char in char_map:
+            char_map[char] += 1
+        else:
+            char_map[char] = 1
+    return char_map
 
 if __name__ == '__main__':
     main()
